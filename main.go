16785d17e5e08b1f027c46090a00b517f577059d
lsaether <Lsaether@protonmail.com>
2018-04-07 11:49:50 +0200

    added ToHash function

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
 )
 
@@ -9,21 +11,31 @@ type transaction string
 
 // Block contains data of the blockchain.
 type Block struct {
-	index        int
-	previousHash string
-	timestamp    int
-	nonce        int
+	index        uint16
+	timestamp    uint16
+	nonce        uint16
 	transactions []transaction
+	previousHash string
 	hash         string
 }
 
 func (b *Block) ToHash() string {
-	sum := sha256.Sum256([]byte("hello world\n"))
+	uintBytes := make([]byte, 6)
+	binary.LittleEndian.PutUint16(uintBytes[0:], b.index)
+	binary.LittleEndian.PutUint16(uintBytes[2:], b.timestamp)
+	binary.LittleEndian.PutUint16(uintBytes[4:], b.nonce)
+	// for _, x := range b.transactions {
+
+	// }
+	var buf bytes.Buffer
+	buf.Write(uintBytes)
+	buf.Write([]byte(b.previousHash))
+	sum := sha256.Sum256(buf.Bytes())
 	return fmt.Sprintf("%x", sum)
 }
 
 func main() {
-	b := new(Block)
+	b := Block{previousHash: "somedatah3re", index: 3}
 	val := b.ToHash()
 	fmt.Println(val)
 }
